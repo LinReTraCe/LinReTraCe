49ce2395a81dee9cb53ac808f8a45cdb5b427d0f
Matthias Pickem <matthias.pickem@gmail.com>
2022-03-15 20:55:54 +0100

    lrtc: bugfix find_DFT chemical potential sometimes landed below gap

--- src_linretrace/root.F90
+++ src_linretrace/root.F90
@@ -59,8 +59,9 @@ subroutine find_mu_DFT(edisp,kmesh,pot)
      call occ_DFT(mu,occ,edisp,kmesh)
      target_zero = edisp%nelect - occ
 
-     if ((mu2-mu1)/2.d0 .lt. abort) exit ! abort if the difference is smaller than the possible
-                                         ! changes caused by the kmesh
+     if ((mu2-mu1)*5.d0 .lt. abort) exit ! abort if the difference is smaller than the possible
+       ! changes caused by the kmesh
+       ! makes sure that the chemical potential always lands inside the gap and ned minimally below it
 
      if (target_zero.gt.0.q0) then
         mu1=mu
