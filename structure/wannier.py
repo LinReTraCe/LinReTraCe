7f1454d7b9b5b91d7787fb469cb61535b3b4cec5
Matthias Pickem <matthias.pickem@gmail.com>
2022-04-26 13:10:20 +0200

    Peierls correction term documented equation

--- structure/wannier.py
+++ structure/wannier.py
@@ -334,8 +334,9 @@ class Wannier90Calculation(DftCalculation):
       if peierlscorrection:
         # Jan's code snippet
         # generalized Peierls for multi-atomic unit-cells (and, obviously, supercells)
-        distances = self.plist[:,None,:] - self.plist[None,:,:] # nproj nproj 3
-        ri_minus_rj = np.einsum('id,abi->abd', self.rvec, distances)
+        # correction term: +1j (rho_l^alpha - rho_l'^alpha) H_ll' (k)
+        distances = self.plist[:,None,:] - self.plist[None,:,:] # nproj nproj 3 -- w.r.t basis vectors
+        ri_minus_rj = np.einsum('id,abi->abd', self.rvec, distances) # cartesian directions
         hvk_correction = 1j * hk[:,:,:,None] * ri_minus_rj[None,:,:,:]
         hvk += hvk_correction
 
