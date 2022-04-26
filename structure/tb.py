d81fd363891aae88341a444e3d28d30cd4afb1fd
Matthias Pickem <matthias.pickem@gmail.com>
2022-04-26 17:00:37 +0200

    tb.py: complex intra-band values raise error

--- structure/tb.py
+++ structure/tb.py
@@ -235,10 +235,9 @@ class TightBinding(Model):
 
       ''' re-interpret sign of local energies '''
       ir = np.argwhere(np.sum(np.abs(self.rpoints-rvec[None,:]),axis=1)==0)[0][0]
-      if np.all(rvec==np.zeros((3,), dtype=np.int)) and orb1==orb2:
+      if orb1==orb2:
         if hop.imag:
-          logger.warning('\nDetected complex local intra-band energy, truncating!\n{} -> {}\n'.format(hop,hop.real))
-          hop = hop.real
+          raise IOError('\nDetected complex intra-band values \n{} in line \n{}\nFix input file.'.format(hop,self.tbdata[i]))
         hop *= (-1)
       self.hr[ir,orb1-1,orb2-1] += -hop
 
