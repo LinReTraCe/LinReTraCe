c324fffcb26b1bb55e8924daf5ca909c24d31aa4
Matthias Pickem <matthias.pickem@gmail.com>
2022-09-29 11:43:53 +0200

    Testsuite: Improved precision tests

---
+++ testsuite/validation/01_test_precision.f90
@@ -0,0 +1,43 @@
+program precisiontest
+  implicit none
+
+  write(*,*) 'begin quadruple precision tests'
+  call double()
+  call quad()
+  write(*,*) 'end quadruple precision tests'
+
+  contains
+
+  subroutine double()
+    implicit none
+    integer, parameter :: dp = selected_real_kind(15, 307)
+
+    write(*,'(A)', advance='no') '      double precision:'
+    if (dp .eq. 8) then
+      write(*,*) 'success'
+    else
+      write(*,*) 'failed'
+      write(*,*) '-------------------------------------------'
+      write(*,*) 'Compiler does not support double precision.'
+      write(*,*) '-------------------------------------------'
+      stop
+    endif
+  end subroutine
+
+  subroutine quad()
+    implicit none
+    integer, parameter :: qp = selected_real_kind(33, 4931)
+
+    write(*,'(A)', advance='no') '      quad precision precision:'
+    if (qp .eq. 16) then
+      write(*,*) 'success'
+    else
+      write(*,*) 'failed'
+      write(*,*) '-----------------------------------------'
+      write(*,*) 'Compiler does not support quad precision.'
+      write(*,*) '-----------------------------------------'
+      stop
+    endif
+  end subroutine
+
+end program
